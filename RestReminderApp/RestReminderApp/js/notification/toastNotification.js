524df195d4c7a1683c42e00c38685faec8dd29ae
vaster <telerik.vasko@gmail.com>
2013-11-02 09:19:46 +0200

    added sample toast notification

---
+++ RestReminderApp/RestReminderApp/js/notification/toastNotification.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+(function () {
+    var callSaveFileNotification = function () {
+        var template = notifications.ToastTemplateType.toastText01;
+        var toast = notifications.ToastNotificationManager.getTemplateContent(template);
+        toast.createElement("text");
+        var text = toast.getElementsByTagName("text");
+        text.getAt(0).innerText = "File saved succsesfully.";
+        var toast = new notifications.ToastNotification(toast);
+        var toastNotifier = notifications.ToastNotificationManager.createToastNotifier();
+        toastNotifier.show(toast);
+    }
+
+    WinJS.Namespace.define("OCR.Notifications", {
+        activateFileSavedNotification: callSaveFileNotification,
+    });
+})()
