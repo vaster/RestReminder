a48f9161b1755e40bd060e03bb289a6fe96b689c
Daniel <Drgreen_bg@yahoo.com>
2013-11-02 02:02:55 -0700

    Added data-layer

---
+++ RestReminderApp/RestReminderApp/js/Data/data-layer.js
@@ -0,0 +1,45 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+(function () {
+    var eventsArray = [];
+
+    function getAllEvents() {
+        return eventsArray;
+    }
+
+    function getEventById(Id) {
+        var eventToReturn = null;
+
+        eventsArray.forEach(function (event) {
+            if (event.Id === Id) {
+                benchToReturn = event;
+            }
+        });
+
+        return eventToReturn;
+    }
+
+    var addEvent = function (eventDTO) {
+        return new WinJS.Promise(function (success, error, progrss) {
+            // ReWrite AJAX
+            eventsArray.push(eventDTO);
+            success();
+        }).then(function () {
+            WinJS.Navigation.navigate("/view/home/home.html");
+        });
+    }
+
+    var addLoadedEvent = function (eventDTO) {
+        return new WinJS.Promise(function (success, error, progrss) {
+            eventsArray.push(eventDTO);
+            success();
+        });
+    }
+
+    WinJS.Namespace.define("Data", {
+        getAllEvents: getAllEvents,
+        getEventById: getEventById,
+        addEvent: addEvent,
+        addLoadedEvent: addLoadedEvent
+    });
+
+})();
